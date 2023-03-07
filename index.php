15d827cc2deb7fdacb0df57eeb24b3abf06dfbee
All4nP <66078686+4llanP@users.noreply.github.com>
2023-03-07 14:17:27 -0300

    php intercalado com html

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php $nome= "Allan";?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Site em PHP de <?=$nome?></title>
+    </head>
+
+    <body>
+        <h1>Meu nome é <?php echo $nome;?></h1>
+        <h2>Olá <?php print($nome);?>!</h2>
+    </body>
+
+</html>
